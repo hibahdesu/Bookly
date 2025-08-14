6e7274bac623d919400fa015640cde0ca4a75823
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-08-14 20:49:36 +0300

    Update the auth

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -4,7 +4,7 @@ from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
 
-class AccessTokenBearer(HTTPBearer):
+class TokenBearer(HTTPBearer):
     
     def __ini__(self, auto_error = True):
         super().__init__(auto_error=auto_error)
@@ -21,16 +21,14 @@ class AccessTokenBearer(HTTPBearer):
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail='Invalid or expired token'
             )
+        
+        self.verify_token_data(token_data)
 
         # print(creds.scheme)
 
         # print(creds.credentials)
 
-        if token_data['refresh']:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail='Please provide an access token'
-            )
+        
 
         return token_data
     
@@ -40,5 +38,26 @@ class AccessTokenBearer(HTTPBearer):
         token_data = decode_token(token)
 
         return True if token_data is not None else False
-
     
+    def verify_token_data(self, token_data):
+        raise NotImplementedError("Please Override this method in child classes")
+    
+
+class AccessTokenBearer(TokenBearer):
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data['refresh']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail='Please provide an access token'
+        )
+
+
+
+class RefreshTokenBeare(TokenBearer):
+     def verify_token_data(self, token_data: dict) -> None:
+        if token_data and not token_data['refresh']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail='Please provide an refresh token'
+        )
+
