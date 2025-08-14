9c9fff67ce3576e9c49091a6a638138a72b7ef3b
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-08-14 21:01:28 +0300

    Update the access

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -8,6 +8,7 @@ from .utils import create_access_token, decode_token, verify_password
 from fastapi.responses import JSONResponse
 from datetime import timedelta
 from .dependencies import RefreshTokenBearer
+from datetime import datetime
 
 
 
@@ -77,8 +78,20 @@ async def login_users(login_data: UserLoginModel, session: AsyncSession = Depend
 
 @auth_router.get('/refresh_token')
 async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details['exp']
+
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(
+            user_data=token_details['user']
+        )
+
+        return JSONResponse(content={
+            "access_token": new_access_token
+        })
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                        detail="Invalid or expired token")
 
 
-
-    return {}
+    
 
