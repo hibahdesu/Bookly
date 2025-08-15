2058d83df48171ff57cc232912fcf46e6ef59697
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-08-15 17:53:27 +0300

    Adding the review logic

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from src.books.routes import book_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
 from src.auth.routes import auth_router
+from src.reviews.routes import review_router
 
 
 @asynccontextmanager
@@ -22,3 +23,4 @@ app = FastAPI(
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
 app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=['auth'])
+app.include_router(review_router, prefix=f"/api/{version}/reviews", tags=['reviews'])
--- src/db/models.py
+++ src/db/models.py
@@ -41,6 +41,7 @@ class User(SQLModel, table=True):
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     books: List["Book"] = Relationship(back_populates='user', sa_relationship_kwargs={'lazy': 'selectin'})
+    reviews: List["Review"] = Relationship(back_populates='user', sa_relationship_kwargs={'lazy': 'selectin'})
 
 
     def __repr__(self):
@@ -73,9 +74,41 @@ class Book(SQLModel, table=True):
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     user: Optional[User] = Relationship(back_populates='books')
+    reviews: List["Review"] = Relationship(back_populates='book', sa_relationship_kwargs={'lazy': 'selectin'})
 
 
     def __repr__(self):
         return f"<BOOK {self.title}>"
+    
+
+
+
+    
+class Review(SQLModel, table=True):
+    __tablename__ = 'reviews' 
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+    rating: int = Field(lt=5)
+    review_text: str
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
+    book_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="books.uid")
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    user: Optional[User] = Relationship(back_populates='reviews')
+    book: Optional[Book] = Relationship(back_populates='reviews')
+
+
+    def __repr__(self):
+        return f"<Review for book {self.book_uid} by user {self.user}>"
+    
+
+
 
     
