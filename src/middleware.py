f15dcfc8e5bd6a722a2f60a74da24082eb6ae4b8
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-08-16 18:18:27 +0300

    Update the middleware

--- src/middleware.py
+++ src/middleware.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+from fastapi.requests import Request
+import time
 
 def register_middleware(app: FastAPI):
-    pass
+    
+    @app.middleware('http')
+    async def custom_logging(request: Request, call_next):
+        start_time = time.time()
+        print('before', start_time)
+
+        response = await call_next(request)
+        processing_time = time.time() - start_time
+        print('Processed after', processing_time)
+
+        return response
