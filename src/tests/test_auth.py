17498c86282f6d2a68913f019711227d05db0b8b
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2025-08-19 14:00:53 +0300

    Update test_auth

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -1,9 +1,11 @@
+from src.auth.schemas import UserCreateModel
+
 
 auth_prefix = f"/api/v1/auth"
 
 def test_user_creation(fake_session, fake_user_service, test_client):
     signup_data = {
-            "first_name": "hibah",
+            "first_name": "yuki",
             "last_name": "san",
             "email": "yuki@gmail.com",
             "username": "david124",
@@ -14,5 +16,9 @@ def test_user_creation(fake_session, fake_user_service, test_client):
         json=signup_data
     )
 
+    user_data = UserCreateModel(**signup_data)
+
     assert fake_user_service.user_exists_called_once()
     assert fake_user_service.user_exists_called_once_with(signup_data['email'], fake_session)
+    assert fake_user_service.create_user_called_once()
+    assert fake_user_service.create_user_called_once_with(user_data, fake_session)
